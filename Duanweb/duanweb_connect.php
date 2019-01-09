639015bf158042ea6cd6bfd4d40a20d62660a2a7
lnsang <zlnsangz@gmail.com>
2019-01-09 18:49:08 +0700

    Revert "First commit"
    This reverts commit 8939bdf9f2a286591bcdfbdc4a7e6abea3bd8a99.

--- Duanweb/duanweb_connect.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-	$kn = mysqli_connect("localhost","root","","duanweb");
-	mysqli_query($kn,"set names utf8");
-?>
--- Duanweb/update_lt.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	session_start();
-	include 'duanweb_connect.php';
-	$sql22 = "update tbdethi set luotthi=luotthi+1 where madt='".$_SESSION['tendted']."'";
-	$kq11 = mysqli_query($kn,$sql22);
-?>
